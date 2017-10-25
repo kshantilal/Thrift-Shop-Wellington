6b8e1b8f52cc4b7904bfd1f58506626c9c2c80f8
Kunal <kshantilal03@gmail.com>
2017-10-25 20:58:43 +1300

    adding first files onto github

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php 
+	
+	function customThemeEnqueues(){
+		wp_enqueue_style( 'bootstrapCSS', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.7', 'all' );
+		wp_enqueue_style( 'customStyle', get_template_directory_uri() . '/css/thriftshop.css', array(), '1.0.0', 'all' );
+
+		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script( 'bootstrapJS', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '1.0.0', true );
+		wp_enqueue_script( 'customScript', get_template_directory_uri() . '/js/thriftshop.js', array(), '1.0.0', true ); //true is asking is it in the footer. true or false.
+
+	}
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Thrift Shop Wellington</title>
+</head>
+<body>
+
+	<h1>My thrift shop</h1>
+
+</body>
+</html>
